299bc93902bae47ec453cf15ed6445c99eec3587
Sayan Shankhari <sayan0shankhari@gmail.com>
2026-08-10 01:50:29 +0530

    fix(misc): add color profiles
    Use common colorlib exported environment path.
    Fix regular expressions.
    Add color profiles.

--- lua/colordot/parser.lua
+++ lua/colordot/parser.lua
@@ -1,5 +1,6 @@
 local P = {};
 
+local colorlib = require ("colorlib");
 
 -- keep the PATTERNS biggest to smallest string
 local PATTERNS = {
@@ -14,8 +15,13 @@ local PATTERNS = {
     format = "COLOR_SHORT_xRGB",
     pattern = "^#%x%x%x$",
     convert = function (lexeme)
-      local r, g, b = lexeme:match ("^#(%x)(%x)(%x)$");
-      return string.format ("#%s%s%s%s%s%s", r,r, g,g, b,b):lower();
+      local xr, xg, xb = lexeme:match ("^#(%x)(%x)(%x)$");
+      local xrgb = string.format ("#%s%s%s%s%s%s", xr,xr, xg,xg, xb,xb):lower();
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.xRGB, bit_depth = 8 },
+        channels = { xrgb = xrgb },
+      };
+      if colorlib.valid (color) then return xrgb end;
     end,
   },
  {
@@ -23,8 +29,15 @@ local PATTERNS = {
     pattern = "^[rR][gG][bB]%((%d+),(%d+),(%d+)%)$",
     convert = function (lexeme)
       local r, g, b = lexeme:match ("^[rR][gG][bB]%((%d+),(%d+),(%d+)%)$");
-      r, g, b = r % 255, g % 255, b % 255;
-      return string.format ("#%02x%02x%02x", r, g, b);
+      r, g, b = r % 256, g % 256, b % 256;
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.RGB, bit_depth = 8 },
+        channels = { r = r, g = g, b = b },
+      };
+      if colorlib.valid (color) then
+        local xrgb = colorlib.conversions.rgb_to_xrgb (r, g, b);
+        return xrgb;
+      end
     end,
   },
  {
@@ -35,24 +48,78 @@ local PATTERNS = {
       local sr = tonumber (sr_str);
       local sg = tonumber (sg_str);
       local sb = tonumber (sb_str);
-      local r = math.floor (255 * sr) & 0xff;
-      local g = math.floor (255 * sg) & 0xff;
-      local b = math.floor (255 * sb) & 0xff;
-      return string.format ("#%02x%02x%02x", r, g, b);
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.sRGB },
+        channels = { sr = sr, sg = sg, sb = sb },
+      };
+      if colorlib.valid (color) then
+        local xrgb = colorlib.conversions.srgb_to_xrgb (sr, sg, sb);
+        return xrgb;
+      else
+        return "#000000";
+      end
     end,
   },
   {
     format = "COLOR_lRGB",
-    pattern = "^l[rR][gG][bB]%(([%d%.]+),([%d%.]+),([%d%.]+)%)$",
+    pattern = "^l[rR][gG][bB]%(([%d%.]+%%?),([%d%.]+%%?),([%d%.]+%%?)%)$",
     convert = function (lexeme)
-      local lr_str, lg_str, lb_str = lexeme:match ("^l[rR][gG][bB]%(([%d%.]+),([%d%.]+),([%d%.]+)%)$");
+      local lr_str, lg_str, lb_str = lexeme:match ("^l[rR][gG][bB]%(([%d%.]+%%?),([%d%.]+%%?),([%d%.]+%%?)%)$");
       local lr = tonumber (lr_str);
       local lg = tonumber (lg_str);
       local lb = tonumber (lb_str);
-      local r = math.floor (255 * lr) & 0xff;
-      local g = math.floor (255 * lg) & 0xff;
-      local b = math.floor (255 * lb) & 0xff;
-      return string.format ("#%02x%02x%02x", r, g, b);
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.lRGB },
+        channels = { lr = lr, lg = lg, lb = lb },
+      };
+      if colorlib.valid (color) then
+        local xrgb = colorlib.conversions.lrgb_to_xrgb (lr, lg, lb);
+        return xrgb;
+      else
+        return "#000000";
+      end
+    end,
+  },
+  {
+    format = "COLOR_HSL",
+    pattern = "^[hH][sS][lL]%((%d+%.?%d*),(%d+%.?%d*%%?),(%d+%.?%d*%%?)%)$",
+    convert = function (lexeme)
+      local h_str, s_str, l_str = lexeme:match ("^[hH][sS][lL]%((%d+%.?%d*),(%d+%.?%d*%%?),(%d+%.?%d*%%?)%)$");
+      s_str, _ = s_str:gsub ("%%$", "");
+      l_str, _ = l_str:gsub ("%%$", "");
+      local h = tonumber (h_str);
+      local s = tonumber (s_str);
+      local l = tonumber (l_str);
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.HSL },
+        channels = { h = h, s = s, l = l },
+      };
+      if colorlib.valid (color) then
+        local xrgb = colorlib.conversions.hsl_to_xrgb (h, s, l);
+        return xrgb;
+      else
+        return "#000000";
+      end
+    end,
+  },
+  {
+    format = "COLOR_nHSL",
+    pattern = "^n[hH][sS][lL]%((%d+%.?%d*),(%d+%.?%d*),(%d+%.?%d*)%)$",
+    convert = function (lexeme)
+      local nh_str, ns_str, nl_str = lexeme:match ("^n[hH][sS][lL]%((%d+%.?%d*),(%d+%.?%d*),(%d+%.?%d*)%)$");
+      local nh = tonumber (nh_str);
+      local ns = tonumber (ns_str);
+      local nl = tonumber (nl_str);
+      local color = {
+        metadata = { profile = colorlib.registry.Profile.nHSL },
+        channels = { nh = nh, ns = ns, nl = nl },
+      };
+      if colorlib.valid (color) then
+        local xrgb = colorlib.conversions.nhsl_to_xrgb (nh, ns, nl);
+        return xrgb;
+      else
+        return "#000000";
+      end
     end,
   },
 };
